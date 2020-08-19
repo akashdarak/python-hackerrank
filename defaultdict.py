32be69b56003413b228ca0de6f4ab52ffa26d065
Akash Darak <akashdarak@gmail.com>
2020-08-19 18:12:20 -0400

    Update defaultdict.py

--- defaultdict.py
+++ defaultdict.py
@@ -1,1 +1,29 @@
 
+# Read input from STDIN. Print output to STDOUT
+import sys
+import os
+from collections import defaultdict
+
+lin = []
+lim = []
+
+n, m = input().split()
+
+#for line in sys.stdin:
+#    print(line)
+
+count = 0
+for line in sys.stdin:
+    count += 1
+    line = line.strip()
+    lin.append(line)
+    if int(count) == int(n):
+        break
+print(lin)
+
+for line in sys.stdin:
+    line = line.strip()
+    lim.append(line)
+print(lim)
+
+#d = defaultdict(lambda: "-1")
