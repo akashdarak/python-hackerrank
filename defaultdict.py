d09b65e9de12b96049fb6aa5d3e03978df0e4263
Akash Darak <akashdarak@gmail.com>
2020-08-20 10:54:17 -0400

    Update defaultdict.py

--- defaultdict.py
+++ defaultdict.py
@@ -1,5 +1,5 @@
-
 # Read input from STDIN. Print output to STDOUT
+
 import sys
 import os
 from collections import defaultdict
@@ -19,11 +19,15 @@ for line in sys.stdin:
     lin.append(line)
     if int(count) == int(n):
         break
-print(lin)
 
 for line in sys.stdin:
     line = line.strip()
     lim.append(line)
-print(lim)
+
+for k in range(len(lim)):
+    indices = [i for i, x in enumerate(lin) if x == lim[k]]
+    for j in range(len(indices)):
+        print(int(indices[j])+1, end=" ")
+    print("")
 
 #d = defaultdict(lambda: "-1")
