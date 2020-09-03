874e80936a6b194b75d2bbbf67f76cb912bc514c
Akash Darak <akashdarak@gmail.com>
2020-09-02 22:32:17 -0400

    Update regex.py

--- regex.py
+++ regex.py
@@ -4,8 +4,21 @@ import re
 s = input()
 k = input()
 
-for i in range(len(s)):
-    s = s[i, i + int(len(k))]
-    m = re.search(k, s)
-    print(m.start())
-    print(m.end())
+def test():
+    for i in range(len(s)):
+        s = s[i, i + int(len(k))]
+        m = re.search(k, s)
+        print(m.start())
+        print(m.end())
+    
+
+def working():
+    temp = 0
+    for i in range(len(s)):
+        if (s[i:i+len(k)]) == k:
+            print('({}, {})'.format(i,i+len(k)-1))
+            temp = 1
+    
+    if temp == 0:
+        print("(-1, -1)")
+
