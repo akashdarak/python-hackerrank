45214b8287769a0092df379f0482089e5f71131b
Akash Darak <akashdarak@gmail.com>
2020-09-10 22:54:45 -0400

    Update addTwoDigits.py

--- addTwoDigits.py
+++ addTwoDigits.py
@@ -4,6 +4,24 @@ def addTwoDigits(n):
         sum = sum + int(n%10)
         n = int(n/10)
     return sum
+
+def digitsManipulations(n):
+    m = n
+    
+    prod = 1
+    while(n != 0):
+        prod = prod*(int(n%10))
+        n = int(n/10)
+    
+    total = 0
+    while(m != 0):
+        total = total + int(m%10)
+        m = int(m/10)
+    
+    final = prod - total
+    return final
     
 n = 92
 print(addTwoDigits(n)) 
+
+print(digitsManipulations(n))
