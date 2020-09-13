086dc1ad399142c3d3e7a5b8cae19f3f62309107
Akash Darak <akashdarak@gmail.com>
2020-09-13 13:39:37 -0400

    Create twosum.py

---
+++ twosum.py
@@ -0,0 +1,12 @@
+def twosum(arr, z):
+    final = []
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] + arr[j] == z:
+                final.append([arr[i], arr[j]])
+    return final
+ 
+arr = [2,7,3,11,5,4,6]
+z=9 
+
+print(twosum(arr,z))
