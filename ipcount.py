3c26e6c86a137dc41b182e82339916af4ace52d4
Akash Darak <akashdarak@gmail.com>
2020-09-23 16:09:07 -0400

    Update ipcount.py

--- ipcount.py
+++ ipcount.py
@@ -1,3 +1,18 @@
+#We suspect we are under an SSH brute-force attack from a small number of remote IP addresses. 
+#We would like to analyze the appropriate log file to get a count of failed login attempts from each remote IP address. 
+#How would you do this with a simple script in Python/Bash/(your preferred language) given the following sample log entry:
+
+#log.txt
+#Jan 30 11:08:25 prod-db-01 sshd[30067]: Failed password for hacker from 192.168.1.2 port 43367 ssh2
+#Jan 30 11:08:25 prod-db-01 sshd[30067]: Failed password for hacker from 192.168.1.3 port 43367 ssh2
+#Jan 30 11:08:25 prod-db-01 sshd[30067]: Failed password for hacker from 192.168.1.2 port 43367 ssh2
+#Jan 30 11:08:25 prod-db-01 sshd[30067]: Failed password for hacker from 192.168.1.4 port 43367 ssh2
+
+#Output
+#192.168.1.2 2
+#192.168.1.3 1
+#192.168.1.4 1
+
 import re
 
 with open("log.txt") as file:
