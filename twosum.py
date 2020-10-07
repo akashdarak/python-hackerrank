bd4eaf97ec9ec0973360918b3629b9cc53caf1b6
Akash Darak <akashdarak@gmail.com>
2020-10-07 14:44:51 -0400

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,6 +1,6 @@
 def twosum(arr, z):
     final = []
-    for i in range(len(arr)):
+    for i in range(len(arr)-1):
         for j in range(i+1, len(arr)):
             if arr[i] + arr[j] == z:
                 final.append([arr[i], arr[j]])
